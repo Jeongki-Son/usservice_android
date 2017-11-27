e636aa9e6beb5b0887496f310ba2e91e59e19832
Jeongki-Son <force0429@gmail.com>
2017-11-27 21:59:40 +0900

    post card data to server

--- app/src/main/java/com/seoultechus/us/usservice/AppData.java
+++ app/src/main/java/com/seoultechus/us/usservice/AppData.java
@@ -7,6 +7,10 @@ package com.seoultechus.us.usservice;
 public class AppData {
     public static final String host = "https://usservice-ghkdgh2365.c9users.io";
 
+    // card routes
+    public static final String postCard = host + "/cards";
+
     // receipt routes
     public static final String postReceipt = host + "/receipts";
+
 }
