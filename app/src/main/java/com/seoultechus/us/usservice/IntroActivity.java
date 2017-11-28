eb4d6c5fe97d6d9e6303de64700a395eb07892dc
Jeongki-Son <force0429@gmail.com>
2017-11-29 02:47:13 +0900

    get web token from server

---
+++ app/src/main/java/com/seoultechus/us/usservice/IntroActivity.java
@@ -0,0 +1,31 @@
+package com.seoultechus.us.usservice;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.os.Handler;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class IntroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(IntroActivity.this, LoginActivity.class);
+                startActivity(intent);
+
+                finish();
+            }
+        }, 2000);
+    }
+}
